c08cbd9dd7162ba6a35dd0af5c5a9227fa812bb0
Drimmark <drimmark@Midgard.Yggdrasil>
2014-09-02 16:25:25 +0200

    Creando el esqueleto

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Define ABSPATH as this files directory. */
+define( 'ABSPATH', dirname(__FILE__) . '/' );
+
+/* Roles mas privilegios = un numero mas grande. */
+define('ROL_USUARIO', 10);
+define('ROL_ADMIN', 100);
+
+// LDAP Parameters
+define('LDAP_HOST', 'ldaps://ldap.uc3m.es');
+define('LDAP_BASEDN', 'ou=Alumnos,ou=Gente,o=Universidad Carlos III,c=es');
+define('LDAP_IDFIELD', 'uid');
+define('LDAP_NAMEFIELD', 'cn');
+define('LDAP_MAILFIELD', 'mail');
+define('LDAP_MAILALIASFIELD', 'uc3mcorreoalias');
+date_default_timezone_set('Europe/Madrid');
+
+/* SQL Parameters */
+define('SQL_HOST', 'localhost');
+define('SQL_DB', 'debate');
+define('SQL_PASSWD', 'debate');
+define('SQL_USER', 'debate');
+define('SQL_PORT', 3306);

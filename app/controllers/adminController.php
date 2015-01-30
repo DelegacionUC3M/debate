81583a318a65f0dba94d76432d79eb9fc201d244
drimmark <salen099@gmail.com>
2015-01-30 14:54:56 +0100

    añadiendo admin

--- app/controllers/adminController.php
+++ app/controllers/adminController.php
@@ -13,7 +13,7 @@ class adminController extends Controller {
 
 	//Función necesaria para que el caso de que la seguridad falle? exista el método panel de this.
 	function panel() {
-		/*if(!$this->security(false)) header('Location: /debate/inicio');	
+		if(!$this->security(false)) header('Location: /debate/inicio');	
 		$preguntas = new Pregunta();
 
 		//Solo se quieren poder borrar preguntas, los if para like o introducir preguntas sobrarían.
@@ -23,59 +23,12 @@ class adminController extends Controller {
 			$preguntas = Pregunta::findById($_POST['pregunta_like']);
 			$preguntas[0]->remove();
 		}
-*/
-		//PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAA
-		if(!$this->security(false)) header('Location: /debate/inicio');	
-		$preguntas = new Pregunta();
-		/* No va a postear preguntas 
-		if (isset($_POST['pregunta'])) {
-			if(!empty($_POST['pregunta'])){
-				if($preguntas->isSetText(htmlspecialchars($_POST['pregunta']))){
-					$data['error'] = 'Ya existe la pregunta';
-				}else{
-					$preguntas->uid = $_SESSION['user']->uid;
-					$preguntas->author = $_SESSION['user']->cn;
-					$preguntas->text = htmlspecialchars($_POST['pregunta']);
-					$preguntas->category = $_SESSION['user']->category;
-					$preguntas->likes = 0;
-					$preguntas->save();
-				}
-			}else{
-				$data['error'] = 'Debes escribir una pregunta.';
-			}
-		}
-		*/
-
-		/*No va a darle like a nada
-		if (isset($_POST['like'])) {
-			if($likes->isSetLike($_SESSION['user']->uid,$_POST['pregunta_like'])){
-				$data['error'] = 'Ya has hecho like en esa pregunta.';
-			}else{
-				if($likes->ownLike($_SESSION['user']->uid,$_POST['pregunta_like'])){
-					$data['error'] = 'No puedes hacerte like a ti mismo.';
-				}else{
-					$likes->uid = $_SESSION['user']->uid;
-					$likes->author = $_SESSION['user']->cn;
-					$likes->id_pregunta = $_POST['pregunta_like'];
-					$preguntas->upgradeLikes($likes->id_pregunta,$likes->getLikes($likes->id_pregunta));
-					$likes->save();
-				}
-			}	
-		}
-	*/
-		//Si va a borrar preguntas sin necesidad de comprobar si es el dueño de la pregunta.
-		if (isset($_POST['delete'])) {
-			$preguntas = Pregunta::findById($_POST['pregunta_like']);
-				$preguntas[0]->remove();
-		}
-//PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAA//PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAA//PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAA
-
-
+		
 		//Un array de 3 posiciones en las que cada posición es un array de preguntas de cada grupo.
 		$arrayAlumnos = Pregunta::findByCategory('alumnos');
 		$arrayPDI = Pregunta::findByCategory('pdi');
 		$arrayPAS = Pregunta::findByCategory('pas');
-		$this->render('admin', array('Alumnos' => $arrayAlumnos/*,'PDI' => $arrayPDI,'PAS' => $arrayPAS*/));
+		$this->render('admin', array('Alumnos' => $arrayAlumnos,'PDI' => $arrayPDI,'PAS' => $arrayPAS));
 	}
 
 	function preguntas() {

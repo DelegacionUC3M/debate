2b0fb362c966d4d1257b7883ded4e10710326a61
patataman <lezdshas@gmail.com>
2015-02-03 15:39:03 +0100

    admin

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ $(function(){
 		});
 	});
 
-
+//FRONTAL
 	setInterval(function() {
 		var type = $('#pregunta ul').attr('id');
 		if(type && type.length != 0) {
@@ -50,6 +50,7 @@ $(function(){
 		}
 	}, 2000);
 
+//ADMIN
 	setInterval(function() {
 		var type = $('#pregunta ul').attr('data-type');
 				$.get('/debate/admin/preguntas?type=' + type, function(data) {
@@ -58,7 +59,18 @@ $(function(){
 						$('#pregunta ul').append('<li class="error">No se han encontrado preguntas de esa categoria.</li>');
 					}
 					$.each(data, function() {
-						$('#pregunta ul').append('<li><p>' + this.text + '</p><span>' + this.likes + '</span></li>');
+						$('#pregunta ul').append('<li class='pregunta'>
+						<form class="like" action="/debate/admin/" method="post">
+							<div class="texto">
+								<input type="hidden" name="pregunta_like" value="<?php echo $pregunta->id?>"> <?php echo $pregunta->text?>
+								<p><?php echo $pregunta->likes?></p>
+							</div>
+							<!-- La vista de admin en teoria solo tiene la opción de borrar preguntas, ya que es para 
+							controlarlas y no veo mucho sentido que un admin de like cuando para eso ya tendría su cuenta
+							de la uni -->
+								<button class="trash icon-trash" type="submit" name="delete" value=""></button>
+						</form>
+					</li>');
 					});
 				});
 			

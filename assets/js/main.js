5eec43d824940ccd8ea6db163c6723ee0fae1fca
patataman <lezdshas@gmail.com>
2015-02-03 15:24:06 +0100

    admin

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,5 +49,23 @@ $(function(){
 			});
 		}
 	}, 2000);
+
+	setInterval(function() {
+		var type = $('#pregunta ul').attr('id');
+		if(type && type.length != 0) {
+			if (type!=null){
+				$.get('/debate/admin/preguntas?type=' + type, function(data) {
+				$('#pregunta ul').html('');
+					if(data.length == 0) {
+						$('#pregunta ul').append('<li class="error">No se han encontrado preguntas de esa categoria.</li>');
+					}
+					$.each(data, function() {
+						$('#pregunta ul').append('<li><p>' + this.text + '</p><span>' + this.likes + '</span></li>');
+					});
+				});
+			}
+		}
+	}, 2000);
+
 });
 

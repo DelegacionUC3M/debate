8fc072238618c924bad1b0ab5864452df397efbb
patataman <lezdshas@gmail.com>
2015-02-03 16:17:48 +0100

    inicio

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,19 +37,15 @@ $(function(){
 //FRONTAL
 	setInterval(function() {
 		var type = $('#pregunta ul').attr('id');
-		if(type && type.length != 0) {
 			$.get('/debate/inicio/preguntas?type=' + type, function(data) {
 				$('#pregunta ul').html('');
 				if(data.length == 0) {
 					$('#pregunta ul').append('<li class="error">No se han encontrado preguntas de esa categoria.</li>');
 				}
-				else{
-					$.each(data, function() {
-						$('#pregunta ul').append('<li><p>' + this.text + '</p><span>' + this.likes + '</span></li>');
-					});
-				}
+				$.each(data, function() {
+					$('#pregunta ul').append('<li><p>' + this.text + '</p><span>' + this.likes + '</span></li>');
+				});
 			});
-		}
 	}, 2000);
 
 //ADMIN
@@ -64,8 +60,6 @@ $(function(){
 						$('#pregunta ul').append('<li class="pregunta"><form class="like" action="/debate/admin/" method="post"><div class="texto"><input type="hidden" name="pregunta_like" value="' + this.id +'"> ' + this.text + '<p>' + this.likes + '</p></div><button class="trash icon-trash" type="submit" name="delete" value=""></button></form></li>');
 					});
 				});
-			
-		
 	}, 2000);
 
 });

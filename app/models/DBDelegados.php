6b3509e34d811e4926900c254ddc9f0a7cf94bdd
patataman <lezdshas@gmail.com>
2015-02-06 15:02:11 +0100

    admin

--- app/models/DBDelegados.php
+++ app/models/DBDelegados.php
@@ -1,8 +1,9 @@
 <?php
 class DBDelegados {
-	public static function getRol() {
+	public static function getRol($nia) {
 		$db = new DB(SQL_DB_DELEGADOS);
-		$db->run();
+		$db->run('SELECT permisos.rol FROM permisos LEFT JOIN personas ON personas.id = permisos.id_user
+				where personas.nia = ? AND permisos.app_id = 3;',array($nia));
 		$data = $db->data();
 		return $data[0]['rol'];//Añadir la tabla permisos a la db delegados
 	}
